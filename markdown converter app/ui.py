81b9168cf0e02691e00cb7ccff6fe03a63e8a95c
Vedavarshith868 <vedavarshithc@gmail.com>
2025-02-09 18:23:23 +0530

    Update ui.py

--- markdown converter app/ui.py
+++ markdown converter app/ui.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import requests, os
+import requests
 
 st.set_page_config(page_title="Markdown to HTML Converter", layout="wide")
 
@@ -10,29 +10,19 @@ uploaded_file = st.file_uploader("Choose a Markdown file", type=["md"])
 
 if uploaded_file is not None:
     files = {"file": (uploaded_file.name, uploaded_file.getvalue())}
-    #response = requests.post("https://markdown-to-html-1.onrender.com", files=files) #connecting line
-
-    API_URL = os.getenv("API_URL")  # Get API URL from environment variable
-
-    if API_URL is None:
-        st.error("API_URL is not set. Please configure environment variables.")
-    else:
-        st.write(f"API URL: {API_URL}")
-        response = requests.post(f"{API_URL}", files=files)
-        st.write(f"Response Status: {response.status_code}")
-        st.write(f"Response Text: {response.text}")
+    response = requests.post("http://127.0.0.1:5000/upload", files=files)
     
-        if response.status_code == 200:
-            html_content = response.json().get("html", "")
-            
-            col1, col2 = st.columns(2)
-            
-            with col1:
-                st.subheader("📝 Converted HTML Code")
-                st.code(html_content, language="html")
-            
-            with col2:
-                st.subheader("📌 HTML Preview")
-                st.markdown(html_content, unsafe_allow_html=True)
-        else:
-            st.error("Failed to convert the file. Please try again.")
+    if response.status_code == 200:
+        html_content = response.json().get("html", "")
+        
+        col1, col2 = st.columns(2)
+        
+        with col1:
+            st.subheader("📝 Converted HTML Code")
+            st.code(html_content, language="html")
+        
+        with col2:
+            st.subheader("📌 HTML Preview")
+            st.markdown(html_content, unsafe_allow_html=True)
+    else:
+        st.error("Failed to convert the file. Please try again.")
